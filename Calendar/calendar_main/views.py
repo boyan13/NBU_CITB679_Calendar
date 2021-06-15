6d195dbb90515e2198ae76426dacea110259aa07
Boyan Bonev <bonev1313@gmail.com>
2021-06-16 00:14:18 +0300

    Formatting changes

--- Calendar/calendar_main/views.py
+++ Calendar/calendar_main/views.py
@@ -1,55 +1,55 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.contrib.auth.decorators import login_required
-from calendar_main.models import Event
+import traceback
 import json
-from django.http import HttpResponseNotFound, JsonResponse, response
-from django.views.decorators.csrf import csrf_exempt
 
-def calendar(request):
-    context={
-        'calendar_events': []
-    }
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from django.core.handlers.wsgi import WSGIRequest
+# from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+# from django.contrib.auth.decorators import login_required
+
+from . import models
+
+
+def calendar(request: WSGIRequest):
+    context = {'calendar_events': []}
 
     if request.user.is_authenticated:
-        current_user = request.user
-        events = Event.objects.filter(created_by=current_user.id)
-        context={
-            'calendar_events': events
-        }
+        events = models.Event.objects.filter(created_by=request.user.id)
+        context['calendar_events'] = events
 
     return render(request, "calendar.html", context)
 
+
 @csrf_exempt
 def create(request):
-    current_user = request.user
-    responseObject = {
-        "isSuccessful": False
-    }
+    response = {'isSuccessful': False}
 
     if request.method == "POST":
-        body = json.loads(request.body)
+        req_body = json.loads(request.body)
         try:
             events = []
-            event = Event()
-            for e in body["events"]:
-                event.created_by = current_user.id
-                event.title = e['title']
-                event.day_of_week = 5
-                event.start_time = e['title']
-                event.end_time = e['title']
-                event.for_date = e['title']
-                events.append(event)
+            for ev in req_body["events"]:
+                events.append(models.Event(
+                    created_by=request.user.id,
+                    title=ev['title'],
+                    day_of_week=5,
+                    start_time=ev['title'],
+                    end_time=ev['title'],
+                    for_date=ev['title']
+                ))
 
-            Event.objects.bulk_create(events)
+        except Exception as exc:
+            # Print exception in terminal (since we have no logging)
+            traceback.format_exc(exc)
+            print(exc)
+            # Handle
+            response["isSuccesful"] = False
+            return JsonResponse(response)
 
-            responseObject["isSuccesful"] = True
-        except:
-            responseObject["isSuccesful"] = False
+    return JsonResponse({})
 
-            return JsonResponse(responseObject)
-
-    return JsonResponse()
 
 def update(request):
     return
