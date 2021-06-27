4a8d214015f444f371bd85011ce5853a7676c0c2
Boyan Bonev <bonev1313@gmail.com>
2021-06-27 12:51:45 +0300

    fix html missing commas

--- Calendar/calendar_main/models.py
+++ Calendar/calendar_main/models.py
@@ -5,6 +5,6 @@ from django.conf import settings
 class Event(models.Model):
     title = models.CharField(max_length=255)
     start_date = models.DateTimeField()
-    end_date = models.DateTimeField(default = None, blank = True, null = True)
+    end_date = models.DateTimeField(default=None, blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
